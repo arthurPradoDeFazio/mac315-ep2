ec94d3ce734fc1c3389e0f5307caca5e01613405
Arthur Prado De Fazio <arthur.fazio@usp.br>
2022-10-26 22:25:23 -0300

    add comments

--- B_barra_menosum.m
+++ B_barra_menosum.m
@@ -1,18 +1,16 @@
 0;
 function matrix = B_barinv(Binv, m, u, l) # monta a matriz [B^-1 | u] e faz com que u se transforme no l-ésimo vetor canonico, o que está à esquerda é B barra inversa
   matrix = [Binv u];
-  n_rows = m;
-  n_columns = m + 1;
   
-  for i = 1:n_rows
+  for i = 1:m
     if i == l
-      matrix(l, :) = (1/matrix(l, n_columns)) * matrix(l, :);
+      matrix(l, :) = (1/matrix(l, m + 1)) * matrix(l, :);
     else
-      matrix(i, :) = matrix(i, :) - (matrix(i, n_columns) / matrix(l, n_columns)) * matrix(l, :);
+      matrix(i, :) = matrix(i, :) - (matrix(i, m + 1) / matrix(l, m + 1)) * matrix(l, :);
     endif
   endfor
   
-  matrix(:, [n_columns]) = [];
+  matrix(:, [m + 1]) = [];
 endfunction
 
 function [theta_star, l] = theta(x, bind, m, u) # calcula o theta para andar na direcão básica e o índice em que ele é atingido
@@ -60,8 +58,9 @@ endfunction
 
 function [ind v] = simplex(A, b, c, m, n, x, bind, Binv)
   while 1
-    new_basic_index = entering_column_index(A, c, bind, Binv, m, n);
-    if new_basic_index == -1
+    new_basic_index = entering_column_index(A, c, bind, Binv, m, n); # escolhe índice que vai sair
+
+    if new_basic_index == -1 # nenhum custo reduzido de índices não básicos é negativo -> encotramos a solução
       ind = 0;
       v = x;
       return;
@@ -70,8 +69,8 @@ function [ind v] = simplex(A, b, c, m, n, x, bind, Binv)
     u = Binv * A(:, new_basic_index);
     [theta, exiting_index] = theta_star(x, bind, m, u)
     if theta == Inf
-      ind = 0;
-      v = -u;
+      ind = -1;
+      v = -u; # está errado, não é só -u porque -u só tem índices básicos
       return;
     endif
     
